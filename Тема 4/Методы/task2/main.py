5b7045b1b567decea8f86d34bb8fb1a724bc077a
Selskij <selskij.v@edu.spbstu.ru>
2022-10-26 20:23:42 +0300

    init

--- Тема 4/Методы/task2/main.py
+++ Тема 4/Методы/task2/main.py
@@ -1,11 +1,13 @@
 def is_palindrome(str_):
-    ...  # TODO привести строку к единому регистру и избавиться от пробелов
+    str_new = "".join(str_.lower().split())  # TODO привести строку к единому регистру и избавиться от пробелов
+    str_new = str_.lower()
+    print(str_new)
+    str_new = str_new.split()
+    print(str_new)
+    str_new = ''.join(str_new)
+    print(str_new)
 
-    if ...:  # TODO проверка палиндрома
-        print("Строка палиндром")
-    else:
-        print("Строка не палиндром")
+
 
 
 is_palindrome("Кит на море не романтик")
-is_palindrome("Прочая строка")
